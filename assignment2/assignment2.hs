096fe9d447ccff6ea1d4ddc69996d4b979b3b3cf
lundez <hampus.londogard.866@student.lu.se>
2017-05-02 16:11:57 +0200

    pushing

--- assignment2/assignment2.hs
+++ assignment2/assignment2.hs
@@ -1,12 +1,13 @@
-
+import Data.List(elemIndices)
+import Data.Maybe
 --optimalAlignments :: Int -> Int -> Int -> String -> String -> [AlignmentType]
 
                 -- 2a --
 type ScoreFunction = (Char -> Char -> Int)
 
 similarityScore :: String -> String -> ScoreFunction -> Int
-similarityScore x [] scorer = sum $ map (scorer '-') x
-similarityScore [] y scorer = sum $ map (scorer '-') y
+similarityScore s1 [] scorer = sum $ map (scorer '-') s1
+similarityScore [] s2 scorer = sum $ map (scorer '-') s2
 similarityScore (s1:s1s) (s2:s2s) scorer = maximum [similarityScore s1s s2s scorer + scorer s1 s2, 
                                              similarityScore s1s (s2:s2s) scorer + scorer s1 '-' , 
                                              similarityScore (s1:s1s) s2s scorer + scorer '-' s2]
@@ -25,10 +26,35 @@ test1   = similarityScore s1 s2 scorer1
 check1  = test1 == (-5)
 
                 -- 2b --
--- WTF IS THIS xD
--- So we get xs & ys .. From where? xD
+-- So basically we got tuple (x,y) and we append h1,h2 as headers to this tuple and put it in a list and then return
+-- ie. attachHeads 'h' 'd' [("ej, å")] returns [("hej", "då")]
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])] 
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+                -- 2c --
+maximaBy :: Ord b => (a->b) -> [a] -> [a]
+maximaBy valueFcn xs = map (xs !!)  indexesOfMaxVals
+                     where 
+                     valuesXs         = map valueFcn xs
+                     maxValue         = maximum valuesXs
+                     indexesOfMaxVals = elemIndices maxValue valuesXs 
+
+                -- TEST 2c --
+test2   = maximaBy length ["cs", "efd", "lth", "it"]
+check2  = test2 == ["efd", "lth"]
+
+                -- 2d --
+type AlignmentType = (String, String)
+
+optAlignments :: String -> String -> [AlignmentType]
+optAlignments s1 s2
+
+similarityScore x [] scorer = sum $ map (scorer '-') x
+similarityScore [] y scorer = sum $ map (scorer '-') y
+similarityScore (s1:s1s) (s2:s2s) scorer = maximum [similarityScore s1s s2s scorer + scorer s1 s2, 
+                                             similarityScore s1s (s2:s2s) scorer + scorer s1 '-' , 
+                                             similarityScore (s1:s1s) s2s scorer + scorer '-' s2]
+
 
 
 mcsLength :: Eq a => [a] -> [a] -> Int
