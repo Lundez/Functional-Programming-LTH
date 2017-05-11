36188a9061f959ca5aeb4ced37f48757d8593603
lundez <hampus.londogard.866@student.lu.se>
2017-05-11 12:46:01 +0200

    HEj

--- assignment3/Parser.hs
+++ assignment3/Parser.hs
@@ -4,50 +4,59 @@ module Parser(module CoreParser, T, digit, digitVal, chars, letter, err,
 import Prelude hiding (return, fail)
 import Data.Char
 import CoreParser
-infixl 7 -#, #- 
+infixl 7 -#, #-
 
 type T a = Parser a
 
 err :: String -> Parser a
 err message cs = error (message++" near "++cs++"\n")
 
-iter :: Parser a -> Parser [a]  
-iter m = m # iter m >-> cons ! return [] 
+iter :: Parser a -> Parser [a]
+iter m = m # iter m >-> cons ! return []
 
 cons(a, b) = a:b
 
 (-#) :: Parser a -> Parser b -> Parser b
-m -# n = error "-# not implemented"
+m -# n = m # n >-> snd
 
 (#-) :: Parser a -> Parser b -> Parser a
-m #- n = error "#- not implemented"
+m #- n = m # n >-> fst --error "#- not implemented"
+
+space :: Parser Char
+space = char ? isSpace
 
 spaces :: Parser String
-spaces =  error "spaces not implemented"
+spaces = iter space
 
 token :: Parser a -> Parser a
 token m = m #- spaces
 
+-- String -> (a, String)
 letter :: Parser Char
-letter =  error "letter not implemented"
+letter = char ? isAlpha
+         --(c:cs)
+            -- | isAlpha c    = char (c:cs)
+            -- | otherwise    = err "Not a letter" (c:cs)
 
 word :: Parser String
 word = token (letter # iter letter >-> cons)
 
+ -- String -> Maybe (String, String)
 chars :: Int -> Parser String
-chars n =  error "chars not implemented"
+chars 0 = return []
+chars n = char # chars (n-1) >-> cons
 
 accept :: String -> Parser String
 accept w = (token (chars (length w))) ? (==w)
 
 require :: String -> Parser String
-require w  = error "require not implemented"
+require w = accept w ! (err $ "Error: The strings doesn't match")
 
 lit :: Char -> Parser Char
 lit c = token char ? (==c)
 
-digit :: Parser Char 
-digit = char ? isDigit 
+digit :: Parser Char
+digit = char ? isDigit
 
 digitVal :: Parser Integer
 digitVal = digit >-> digitToInt >-> fromIntegral
@@ -57,4 +66,3 @@ number' n = digitVal #> (\ d -> number' (10*n+d))
           ! return n
 number :: Parser Integer
 number = token (digitVal #> number')
-
--- assignment3/TestParser.hs
+++ assignment3/TestParser.hs
@@ -8,14 +8,14 @@ l1 = letter "abc"   {- Just('a',"bc") -}
 l2 = letter "123"   {- Nothing -}
 l3 = letter ""      {- Nothing -}
 
-w1 = spaces "abc"  {- Just("","abc") -}
-w2 = spaces "  \t abc"  {- Just("  \t ","abc") -}
+w1 = spaces "abc"  {- Just("","abc") -}                     ---
+w2 = spaces "  \t abc"  {- Just("  \t ","abc") -}           ---
 
 c1 = chars 2 "abc"          {-  Just ("ab","c")  -}
 c2 = chars 0 "ab"          {-  Just ("","ab")  -}
 c3 = chars 3 "ab"          {-  Nothing)  -}
 
-r1 = require ":=" ":= 1"     {- Just (":=","1") -}
+r1 = require ":=" ":= 1"     {- Just (":=","1") -}          ---
 r2 = require "else" "then"     {- Program error: expecting else near then -}
 
-a4 = (accept "read" -# word) "read count" {-  Just ("count","") -}
+a4 = (accept "read" -# word) "read count" {-  Just ("count","") -}  ---
